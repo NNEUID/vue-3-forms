9f3a6f8ed72f899756c5ea7efe9bbee1cc5e3d0f
NNEUID <nneuid@gmail.com>
2022-12-26 09:38:09 +0000

    keyboard events & event modifiers

--- src/components/SignupForm.vue
+++ src/components/SignupForm.vue
@@ -8,13 +8,19 @@
 
     <label>Role</label>
     <select v-model="role">
-        <option value="Developer">Web Developer</option>
-        <option value="Designer">Web Designer</option>
+      <option value="Developer">Web Developer</option>
+      <option value="Designer">Web Designer</option>
     </select>
 
+    <label>Skills</label>
+    <input type="text" required v-model="tempSkill" @keyup="addSkill">
+    <div class="pill" v-for="skill in skills" :key="skill">
+      {{ skill }}
+    </div>
+
     <div class="terms">
-        <input type="checkbox" required v-model="terms">
-        <label>Accept Terms and Conditions</label>
+      <input type="checkbox" required v-model="terms">
+      <label>Accept Terms and Conditions</label>
     </div>
 
     <!-- <div class="terms">
@@ -35,59 +41,73 @@
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
+  <p>Skills: {{ skills }}</p>
   <p>Terms Accepted: {{ terms }}</p>
   <!-- <p>Names: {{ names }}</p> -->
 </template>
 
 <script>
 export default {
-    data() {
-        return {
-            email: '',
-            password: '',
-            role: '',
-            terms: false,
-            names: []
-        }
+  data() {
+    return {
+      email: '',
+      password: '',
+      role: '',
+      terms: false,
+      names: [],
+      skills: [],
+      tempSkill: ''
     }
+  },
+  methods: {
+    addSkill(e) {
+      if(e.key === 'Enter' && this.tempSkill) {
+        if(!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill)
+        }
+        this.tempSkill = ''
+      }
+    }
+  }
 }
 </script>
 
 <style>
 form {
-    max-width: 420px;
-    margin: 30px auto;
-    background: white;
-    text-align: left;
-    padding: 40px;
-    border-radius: 10px;
+  max-width: 420px;
+  margin: 30px auto;
+  background: white;
+  text-align: left;
+  padding: 40px;
+  border-radius: 10px;
 }
 
 label {
-    display: inline-block;
-    color: #aaa;
-    margin: 25px 0 15px;
-    font-size: 0.6em;
-    text-transform: uppercase;
-    letter-spacing: 1px;
-    font-weight: bold;
+  display: inline-block;
+  color: #aaa;
+  margin: 25px 0 15px;
+  font-size: 0.6em;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  font-weight: bold;
 }
 
-input, select {
-    display: block;
-    padding: 10px 6px;
-    width: 100%;
-    box-sizing: border-box;
-    border: none;
-    border-bottom: 1px solid #ddd;
-    color: #555;
+input,
+select {
+  display: block;
+  padding: 10px 6px;
+  width: 100%;
+  box-sizing: border-box;
+  border: none;
+  border-bottom: 1px solid #ddd;
+  color: #555;
 }
 
 input[type="checkbox"] {
-    display: inline-block;
-    width: 16px;
-    margin: 0 10px 0 0;
-    position: relative;
-    top: 2px;
+  display: inline-block;
+  width: 16px;
+  margin: 0 10px 0 0;
+  position: relative;
+  top: 2px;
 }
 </style>
